1953a9c1c412ec6f0d365c4a02a7125cc77529cc
Your NameBrunoCauan <brunocaua2000@gmail.com>
2020-11-13 21:38:12 -0300

    Add flying Paimon and copy change

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styles.css";
 
+import paimon from './../../assets/paimon.png';
+
 class Main extends React.Component {
     constructor(props) {
         super(props);
@@ -27,19 +29,24 @@ class Main extends React.Component {
 
         return (
             <div className="container">
+                <div className="image">
+                    <img src={paimon} alt="`Paimon" />
+                </div>
+                
                 <div className="content">
                         <div className="input-group">
-                            <label htmlFor="resin">Resin needed:</label>
+                            <label htmlFor="resin">Let Paimon help you!</label>
                             
                             <input
                                 id="resin"
                                 type="number"
+                                placeholder="How much resin you need?"
                                 value={resin}
                                 onChange={(e) => { this.setState({ resin: e.target.value }) }}
                             />
                         </div>
 
-                        <div className="result">{this.getTotalHours()}</div>
+                        <div className="result">It will take: {this.getTotalHours()}</div>
                 </div>
             </div>
         );
